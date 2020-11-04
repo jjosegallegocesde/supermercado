234af89babc1a85b4a71fa34039a0aa0d7e0ad2c
Juan <juan.gallegomesa@gmail.com>
2020-11-04 16:03:29 -0500

    SE AGREGAN DATOS A LA BD

--- registrarPersonas.php
+++ registrarPersonas.php
@@ -4,15 +4,23 @@ include("BaseDatos.php");
 
 if(isset($_POST["boton"])){
 
+    //1. Recibir los datos
+    $descripcion=$_POST["descripcion"];
     $nombre=$_POST["nombreUsuario"];
     $apellido=$_POST["apellidoUsuario"];
     $cedula=$_POST["cedulaUsuario"];
-    $descripcion=$_POST["descripcion"];
+    
 
-    //echo($nombre."-".$apellido."-".$cedula."-".$descripcion);
-
+    
+    //2. crear un objeto(copia) de la clase BaseDatos()
     $operacionBD= new BaseDatos();
-    $operacionBD->conectarBD();
+   
+
+    //3.Necesito crear una consulta SQL para insertar datos
+    $consultaSQL="INSERT INTO usuarios(nombre,apellido,cedula,descripcion) VALUES ('$nombre','$apellido','$cedula','$descripcion')";
+
+    //4. LLamar al metodo agregarDatos() de la clase BaseDatos
+    $operacionBD->agregarDatos($consultaSQL);
 
 
 
